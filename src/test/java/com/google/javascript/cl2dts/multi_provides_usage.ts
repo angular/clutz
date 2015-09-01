7483c91e35a57b09a367bb1a9f27663b426a3f7d
Alex Eagle <alexeagle@google.com>
2015-09-01 10:54:36 -0700

    fix(bug): look at parents of provides when trimming desired symbols for a namespace.
    fixes #68

--- src/test/java/com/google/javascript/cl2dts/multi_provides_usage.ts
+++ src/test/java/com/google/javascript/cl2dts/multi_provides_usage.ts
@@ -1,3 +1,3 @@
 /// <reference path="./multi_provides" />
-import {three} from 'goog:goog.multi_provides';
-import Two from 'goog:goog.multi_provides.Two';
+import {three} from 'goog:multi_provides.a.b.c';
+import Two from 'goog:multi_provides.a.b.c.Two';
