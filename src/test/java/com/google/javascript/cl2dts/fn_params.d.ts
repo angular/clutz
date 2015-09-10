52b45790e68146a925f509d21c2e610a1753e2ee
Alex Eagle <alexeagle@google.com>
2015-09-10 12:55:00 -0700

    fix(bug): handle closure's representation of functions as classes
    Closes #90

--- src/test/java/com/google/javascript/cl2dts/fn_params.d.ts
+++ src/test/java/com/google/javascript/cl2dts/fn_params.d.ts
@@ -3,6 +3,7 @@ declare namespace ಠ_ಠ.cl2dts_internal.fn_params {
   function optionalNullable (x : string , opt_y ? : number ) : number ;
   function varargs (x : string ,  ...y : ( number ) [] ) : void ;
   function varargs_fns ( ...var_args : ( ( ...a : any [] ) => any ) [] ) : void ;
+  function declaredWithType ( ...a : any [] ) : any ;
 }
 declare module 'goog:fn_params' {
   import alias = ಠ_ಠ.cl2dts_internal.fn_params;
