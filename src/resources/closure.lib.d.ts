7816e442809623680df91646d3e1dd03bf66f910
Martin Probst <martin@probst.io>
2015-10-13 22:24:45 -0700

    chore: missing rename to clutz.

--- src/resources/closure.lib.d.ts
+++ src/resources/closure.lib.d.ts
@@ -1,5 +1,5 @@
 // Work around for https://github.com/Microsoft/TypeScript/issues/983
-// All Cl2dts namespaces are below ಠ_ಠ.cl2dts_internal, thus
+// All clutz namespaces are below ಠ_ಠ.clutz_internal, thus
 // this acts as global.
 declare namespace ಠ_ಠ.clutz_internal {
   type GlobalError = Error;
