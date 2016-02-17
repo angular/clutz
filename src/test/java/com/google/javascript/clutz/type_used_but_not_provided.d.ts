a922ee88cf9a30b613901142deca7511adc995f5
Rado Kirov <radokirov@google.com>
2016-02-16 18:57:15 -0800

    fix: visit fully qualified template types that are used but not provided.
    Allow for recursive discovery of types used but not provided up to
    maximum of 5 iterations.

---
+++ src/test/java/com/google/javascript/clutz/type_used_but_not_provided.d.ts
@@ -0,0 +1,21 @@
+declare namespace ಠ_ಠ.clutz.used.not.provided {
+  var a : ಠ_ಠ.clutz.used.not.provided.C ;
+}
+declare namespace ಠ_ಠ.clutz.goog {
+  function require(name: 'used.not.provided.a'): typeof ಠ_ಠ.clutz.used.not.provided.a;
+}
+declare module 'goog:used.not.provided.a' {
+  import alias = ಠ_ಠ.clutz.used.not.provided.a;
+  export default alias;
+}
+declare namespace ಠ_ಠ.clutz.used.not.provided {
+  class C extends C_Instance {
+  }
+  class C_Instance implements ಠ_ಠ.clutz.used.not.provided.I < number > {
+    private noStructuralTyping_: any;
+  }
+}
+declare namespace ಠ_ಠ.clutz.used.not.provided {
+  interface I < T > {
+  }
+}
