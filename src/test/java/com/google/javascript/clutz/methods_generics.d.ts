a61b9c7a5aab8a0d0ca9e827c05b489e45a2615d
Julien Dramaix <dramaix@google.com>
2016-05-26 13:19:32 +0200

    add test

--- src/test/java/com/google/javascript/clutz/methods_generics.d.ts
+++ src/test/java/com/google/javascript/clutz/methods_generics.d.ts
@@ -1,1 +1,34 @@
-// intentionally empty. We are testing externs conversion
+declare namespace ಠ_ಠ.clutz.method_generics {
+  class Foo < T > extends Foo_Instance < T > {
+    /**
+     * Static method: T and R must be defined on the method in the resulting typescript code
+     */
+    static staticBar < T , R > (bar : R ) : T ;
+  }
+  class Foo_Instance < T > {
+    private noStructuralTyping_: any;
+    constructor (a : T ) ;
+    /**
+     * T is defined on the constructor and should not be redefined on the method level in the resulting
+     * typescript code. R should be defined on the method level.
+     */
+    bar < R > (foo : T , bar : R ) : void ;
+    /**
+     * T is defined on the constructor and should not be redefined on the method level in the resulting
+     * typescript code.
+     */
+    pop ( ) : T ;
+    /**
+     * T is defined on the constructor and should not be redefined on the method level in the resulting
+     * typescript code.
+     */
+    push (value : T ) : void ;
+  }
+}
+declare namespace ಠ_ಠ.clutz.goog {
+  function require(name: 'method_generics'): typeof ಠ_ಠ.clutz.method_generics;
+}
+declare module 'goog:method_generics' {
+  import alias = ಠ_ಠ.clutz.method_generics;
+  export = alias;
+}
--- src/test/java/com/google/javascript/clutz/methods_generics.js
+++ src/test/java/com/google/javascript/clutz/methods_generics.js
@@ -1,1 +1,46 @@
-// intentionally empty. We are testing externs conversion
+goog.provide('method_generics');
+
+/**
+ * @param {T} a
+ * @template T
+ * @constructor
+ */
+method_generics.Foo = function(a) {};
+
+/**
+ * T is defined on the constructor and should not be redefined on the method level in the resulting
+ * typescript code.
+ *
+ * @return {T}
+ * @template T
+ */
+method_generics.Foo.prototype.pop = function() {};
+
+/**
+ * T is defined on the constructor and should not be redefined on the method level in the resulting
+ * typescript code.
+ *
+ * @param {T} value
+ * @template T
+ */
+method_generics.Foo.prototype.push = function(value) {};
+
+/**
+ * T is defined on the constructor and should not be redefined on the method level in the resulting
+ * typescript code. R should be defined on the method level.
+ *
+ * @param {T} foo
+ * @param {R} bar
+ * @template T,R
+ */
+method_generics.Foo.prototype.bar = function(foo, bar) {};
+
+
+/**
+ * Static method: T and R must be defined on the method in the resulting typescript code
+ *
+ * @param {R} bar
+ * @return {T}
+ * @template T,R
+ */
+method_generics.Foo.staticBar = function(bar) {};
