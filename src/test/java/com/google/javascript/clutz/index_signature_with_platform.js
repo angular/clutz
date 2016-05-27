6902969058a8a74350e5e02c4ac5bc0b4d714532
Julien Dramaix <dramaix@google.com>
2016-05-27 14:44:05 +0200

    Better handling of IndexSignatures.
    The IndexSignature is supported in closure by implementing IObject
    interface. IObject has two template types, the first one determines the
    type of the key and the second one determines the type of the returned
    value.
    
    Note that `IArrayLike<T>` extends `IObject<number, T>` but closure
    compiler considers these two interfaces as being independents and a class
    in closure can implements both interfaces. We have to process them
    separately.
    
    Closes #278

---
+++ src/test/java/com/google/javascript/clutz/index_signature_with_platform.js
@@ -0,0 +1,59 @@
+goog.provide('index_signature');
+
+/**
+ * @constructor
+ */
+index_signature.SomeType = function() {}
+
+/**
+ * @constructor
+ * @implements {IArrayLike<string>}
+ */
+index_signature.ImplementsIArrayLike = function() {}
+
+/** @type {number} */
+index_signature.ImplementsIArrayLike.prototype.length;
+
+/**
+ * @constructor
+ * @implements {IObject<string, number>}
+ */
+index_signature.ImplementsIObject = function() {}
+
+/**
+ * @constructor
+ * @template T
+ * @implements {IArrayLike<T>}
+ */
+index_signature.ImplementsIArrayLikeWithGeneric = function() {}
+
+/** @type {number} */
+index_signature.ImplementsIArrayLikeWithGeneric.prototype.length;
+
+/**
+ * @constructor
+ * @template T
+ * @implements {IObject<string, T>}
+ */
+index_signature.ImplementsIObjectWithGeneric = function() {}
+
+/**
+ * @constructor
+ * @extends {index_signature.ImplementsIArrayLike}
+ */
+index_signature.ShouldNotContainIndexSignature = function() {}
+
+/**
+ * @interface
+ * @extends {IArrayLike<string>}
+ */
+index_signature.InterfaceExtendingIArrayLike = function() {};
+
+/**
+ * @constructor
+ * @implements {index_signature.InterfaceExtendingIArrayLike}
+ */
+index_signature.ShouldContainIndexSignature = function() {}
+
+/** @type {number} */
+index_signature.ShouldContainIndexSignature.prototype.length;
