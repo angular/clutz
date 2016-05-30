9fe23395bde83d56336ab65a17f253b73ad50ddc
Julien Dramaix <dramaix@google.com>
2016-05-30 15:02:55 +0200

    code review

--- src/test/java/com/google/javascript/clutz/index_signature_with_platform.js
+++ src/test/java/com/google/javascript/clutz/index_signature_with_platform.js
@@ -2,14 +2,12 @@ goog.provide('index_signature');
 
 /**
  * @constructor
- * @return {!index_signature.SomeType}
  */
 index_signature.SomeType = function() {}
 
 /**
  * @constructor
  * @implements {IArrayLike<string>}
- * @return {!index_signature.ImplementsIArrayLike}
  */
 index_signature.ImplementsIArrayLike = function() {}
 
@@ -19,7 +17,6 @@ index_signature.ImplementsIArrayLike.prototype.length;
 /**
  * @constructor
  * @implements {IObject<string, number>}
- * @return {!index_signature.ImplementsIObject}
  */
 index_signature.ImplementsIObject = function() {}
 
@@ -27,7 +24,6 @@ index_signature.ImplementsIObject = function() {}
  * @constructor
  * @template T
  * @implements {IArrayLike<T>}
- * @return {!index_signature.ImplementsIArrayLikeWithGeneric}
  */
 index_signature.ImplementsIArrayLikeWithGeneric = function() {}
 
@@ -38,14 +34,12 @@ index_signature.ImplementsIArrayLikeWithGeneric.prototype.length;
  * @constructor
  * @template T
  * @implements {IObject<string, T>}
- * @return {!index_signature.ImplementsIObjectWithGeneric}
  */
 index_signature.ImplementsIObjectWithGeneric = function() {}
 
 /**
  * @constructor
  * @extends {index_signature.ImplementsIArrayLike}
- * @return {!index_signature.ShouldNotContainIndexSignature}
  */
 index_signature.ShouldNotContainIndexSignature = function() {}
 
@@ -58,7 +52,6 @@ index_signature.InterfaceExtendingIArrayLike = function() {};
 /**
  * @constructor
  * @implements {index_signature.InterfaceExtendingIArrayLike}
- * @return {!index_signature.ShouldContainIndexSignature}
  */
 index_signature.ShouldContainIndexSignature = function() {}
 
