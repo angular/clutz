37b78e9ccc2fe306cfaa1a2a70fd58e7d0af72ff
David Purpura <dpurp@users.noreply.github.com>
2017-01-22 10:24:34 -0800

    fix: Support classes with undeclared properties  (#435)
    Previously, when gents when gents would encounter an undeclared
    property within a bind statement, it would blow up.

---
+++ src/test/java/com/google/javascript/gents/singleTests/undeclared_property.js
@@ -0,0 +1,13 @@
+goog.provide("foo.Bar");
+
+/**
+ * @constructor
+ */
+foo.Bar = function() {
+};
+
+foo.Bar.prototype.baz = function() {
+  Promise.resolve().then(goog.bind(function(names) {
+    this.undeclaredProperty = {};
+  }, this));
+};
