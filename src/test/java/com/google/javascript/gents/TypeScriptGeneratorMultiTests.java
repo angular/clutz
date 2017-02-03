b61ac6ce621ae4025a8192919412b38f13629092
David Purpura <dpurp@users.noreply.github.com>
2017-02-03 10:16:39 -0800

    Convert to Java 8 (#442)
    Change clutz/gents to use Java8 and run Refaster over the codebase.
    
    Note: closure-compiler is pinned at Guava v20, so not all Guava Java8
    goodness is available yet (v21) adds new convenience methods (namely,
    FluentIterable -> Streams; ImmutableList.toImmutableList (collector))

--- src/test/java/com/google/javascript/gents/TypeScriptGeneratorMultiTests.java
+++ src/test/java/com/google/javascript/gents/TypeScriptGeneratorMultiTests.java
@@ -3,9 +3,6 @@ package com.google.javascript.gents;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 import com.google.common.io.Files;
 import com.google.javascript.clutz.DeclarationGeneratorTests;
 import com.google.javascript.jscomp.SourceFile;
@@ -15,7 +12,10 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -66,9 +66,9 @@ public class TypeScriptGeneratorMultiTests extends TypeScriptGeneratorTests {
         List<File> testFiles = getTestInputFilesRecursive(DeclarationGeneratorTests.JS,
             multiTestPath, dirName);
 
-        Set<String> sourceNames = Sets.newHashSet();
-        List<SourceFile> sourceFiles = Lists.newArrayList();
-        Map<String, String> goldenFiles = Maps.newHashMap();
+        Set<String> sourceNames = new HashSet<>();
+        List<SourceFile> sourceFiles = new ArrayList<>();
+        Map<String, String> goldenFiles = new HashMap<>();
 
         for (final File sourceFile : testFiles) {
           String sourceText = getFileText(sourceFile);
