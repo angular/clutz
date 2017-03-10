d80a268a87a26dff1306baa80f1b3eec0f863b0f
Bowen Ni <bowen1897@gmail.com>
2017-03-10 04:16:37 -0800

    Fix local alias for goog.provide

---
+++ src/test/java/com/google/javascript/gents/singleTests/goog_scope_local_alias.ts
@@ -0,0 +1,12 @@
+
+export class Base1 {}
+
+export class Base2 {}
+
+export class Base3 {}
+let localVarNotGoogProvided = 1;
+
+// localVarNotGoogProvided should not be exported
+localVarNotGoogProvided = localVarNotGoogProvided + 1;
+
+export class Base4 {}
