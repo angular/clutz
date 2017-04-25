5949256c70a21303e9791ad07c2e7a00389ba31e
Bowen Ni <bowen1897@gmail.com>
2017-04-25 13:39:32 -0700

    Upgrade Closure compiler to v20170409

--- src/main/java/com/google/javascript/gents/TypeScriptGenerator.java
+++ src/main/java/com/google/javascript/gents/TypeScriptGenerator.java
@@ -16,6 +16,7 @@ import com.google.javascript.jscomp.DiagnosticType;
 import com.google.javascript.jscomp.ErrorFormat;
 import com.google.javascript.jscomp.JSError;
 import com.google.javascript.jscomp.SourceFile;
+import com.google.javascript.jscomp.ThreadSafeDelegatingErrorManager;
 import com.google.javascript.rhino.Node;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -105,7 +106,7 @@ public class TypeScriptGenerator {
     GentsErrorManager errorManager =
         new GentsErrorManager(
             errStream, ErrorFormat.MULTILINE.toFormatter(compiler, true), opts.debug);
-    compiler.setErrorManager(errorManager);
+    compiler.setErrorManager(new ThreadSafeDelegatingErrorManager(errorManager));
   }
 
   public boolean hasErrors() {
@@ -234,7 +235,7 @@ public class TypeScriptGenerator {
     result.moduleRewriteLog =
         new ModuleRenameLogger()
             .generateModuleRewriteLog(filesToConvert, modulePrePass.getNamespaceMap());
-    ((GentsErrorManager) compiler.getErrorManager()).doGenerateReport();
+    ((ThreadSafeDelegatingErrorManager) compiler.getErrorManager()).generateReport();
     return result;
   }
 
