0055b0be8a3005352480282f3447b7d5ca5f7592
Rado Kirov <rkirov@gmail.com>
2017-05-09 11:29:54 -0700

    Emit inferred props on a goog.module object.
    Clutz walks all `compiler.getTopScope().getAllSymbols()`. It appears
    that for module objects
    
    ```
    goog.module('a.b');
    const c = 0;
    exports.c = c;
    ```
    
    Closure does not register a corresponding 'a.b.c' symbol, while
    
    ```
    goog.module('a.b');
    /** @const */
    const c = 0;
    exports.c = c;
    ```
    
    does produce the symbol in question. To fix the first situation,
    we also traverse the unemitted props on the 'a.b' object.

---
+++ src/test/java/com/google/javascript/clutz/infer_in_goog_module.d.ts
@@ -0,0 +1,8 @@
+declare namespace ಠ_ಠ.clutz.module$exports$infer {
+  var a : string ;
+  var b : string ;
+}
+declare module 'goog:infer' {
+  import alias = ಠ_ಠ.clutz.module$exports$infer;
+  export = alias;
+}
