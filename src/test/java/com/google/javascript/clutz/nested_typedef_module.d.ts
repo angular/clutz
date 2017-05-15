720ec76dcf9dad113d499f8cbe45d510c27d9b90
Rado Kirov <rkirov@gmail.com>
2017-05-15 14:51:08 -0700

    Add a test for inner typedef inside a goog.module.
    Includes a usage file.

---
+++ src/test/java/com/google/javascript/clutz/nested_typedef_module.d.ts
@@ -0,0 +1,17 @@
+declare namespace ಠ_ಠ.clutz {
+  class module$exports$nested$typedef$mod extends module$exports$nested$typedef$mod_Instance {
+  }
+  class module$exports$nested$typedef$mod_Instance {
+    private noStructuralTyping_: any;
+  }
+}
+declare namespace ಠ_ಠ.clutz.module$exports$nested$typedef$mod {
+  type T = { a : string } ;
+}
+declare namespace goog {
+  function require(name: 'module$exports$nested$typedef$mod'): typeof ಠ_ಠ.clutz.module$exports$nested$typedef$mod;
+}
+declare module 'goog:nested.typedef.mod' {
+  import alias = ಠ_ಠ.clutz.module$exports$nested$typedef$mod;
+  export default alias;
+}
---
+++ src/test/java/com/google/javascript/clutz/nested_typedef_module.js
@@ -0,0 +1,10 @@
+goog.module('nested.typedef.mod');
+
+class C {
+
+}
+
+/** @typedef {{a: string}} */
+C.T;
+
+exports = C;
