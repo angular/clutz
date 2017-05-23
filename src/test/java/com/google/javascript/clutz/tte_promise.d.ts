b0db5ade9bb535d387f05292316c422790c9848e
Rado Kirov <rkirov@gmail.com>
2017-05-22 17:52:09 -0700

    Fix TTE generator code to work with instance methods on $q.
    Upgrade to patterns in 1.6 angular typings, including a new TTE for
    Promise.when.
    
    Fixes #505

--- src/test/java/com/google/javascript/clutz/tte_promise.d.ts
+++ src/test/java/com/google/javascript/clutz/tte_promise.d.ts
@@ -7,6 +7,7 @@ declare namespace ಠ_ಠ.clutz.tte {
   class Promise_Instance < T > {
     private noStructuralTyping_: any;
     then < RESULT > (opt_onFulfilled ? : ( (a : T ) => Promise < RESULT > | RESULT ) | null , opt_onRejected ? : ( (a : any ) => any ) | null) : Promise < RESULT > ;
+    when < RESULT, T > (value: T, successCallback: (promiseValue: T) => Promise < RESULT >|RESULT, errorCallback: null | undefined |  ((reason: any) => any), notifyCallback?: (state: any) => any): Promise < RESULT >;
   }
 }
 declare namespace goog {
@@ -14,5 +15,24 @@ declare namespace goog {
 }
 declare module 'goog:tte.Promise' {
   import alias = ಠ_ಠ.clutz.tte.Promise;
+  export default alias;
+}
+declare namespace ಠ_ಠ.clutz.tte {
+  class PromiseService < T > extends PromiseService_Instance < T > {
+  }
+  class PromiseService_Instance < T > {
+    private noStructuralTyping_: any;
+    all(promises : PromiseService.Promise < any > [] ) : PromiseService.Promise < any [] > ;
+  }
+}
+declare namespace ಠ_ಠ.clutz.tte.PromiseService {
+  interface Promise < T > {
+  }
+}
+declare namespace goog {
+  function require(name: 'tte.PromiseService'): typeof ಠ_ಠ.clutz.tte.PromiseService;
+}
+declare module 'goog:tte.PromiseService' {
+  import alias = ಠ_ಠ.clutz.tte.PromiseService;
   export default alias;
 }
