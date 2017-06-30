f555d64b7e55125927744505d0890cc1165c4a4a
Rado Kirov <rkirov@gmail.com>
2017-06-30 15:22:28 -0700

    Switch to accepting ECMASCRIPT_NEXT (#531)
    Adds a test for async/await code.

---
+++ src/test/java/com/google/javascript/clutz/async_await_with_platform.d.ts
@@ -0,0 +1,16 @@
+declare namespace ಠ_ಠ.clutz {
+  class module$exports$asyncawait extends module$exports$asyncawait_Instance {
+  }
+  class module$exports$asyncawait_Instance {
+    private noStructuralTyping_: any;
+    bar ( ) : any ;
+    foo ( ) : any ;
+  }
+}
+declare namespace goog {
+  function require(name: 'module$exports$asyncawait'): typeof ಠ_ಠ.clutz.module$exports$asyncawait;
+}
+declare module 'goog:asyncawait' {
+  import alias = ಠ_ಠ.clutz.module$exports$asyncawait;
+  export default alias;
+}
---
+++ src/test/java/com/google/javascript/clutz/async_await_with_platform.js
@@ -0,0 +1,10 @@
+goog.module('asyncawait');
+
+class C {
+  async foo() {}
+  async bar() {
+     const x = await this.foo();
+  }
+}
+
+exports = C;
