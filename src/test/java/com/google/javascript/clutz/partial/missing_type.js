0cf5caed0f11dee915f80abee257dae055404119
Rado Kirov <rkirov@gmail.com>
2017-09-06 16:36:14 -0700

    Emit missing types as template parameters.
    For unknown to me reason those end up as NamedType instead of NoType,
    thus they need special handling logic.

--- src/test/java/com/google/javascript/clutz/partial/missing_type.js
+++ src/test/java/com/google/javascript/clutz/partial/missing_type.js
@@ -6,5 +6,9 @@ const x = missingFunctionCall();
 /** @const {!goog.missing.map<string, number>} */
 const xWithGenerics = missingFunctionCall2();
 
+/** @const {!goog.missing.map<!mod.ref.A, !mod.ref.B<!mod.ref.C>>} */
+const xWithMissingGenerics = missingFunctionCall3();
+
 exports.x = x;
 exports.xWithGenerics = xWithGenerics;
+exports.xWithMissingGenerics = xWithMissingGenerics;
