66834dfe8e27a60bdebcd5c9a1084184ecd0b92b
Rado Kirov <rkirov@gmail.com>
2017-10-13 14:56:56 -0700

    Adds enum support in gents. (#578)
    For string and number enums convert them to TypeScript, all other
    types of enums are left as object literals.
    
    The comment rewriting pass does not communicate with the pass that
    modifies the AST, so it is not easy to decide whether to strip @enum
    jsdocs or not. We can either always remove them or never remove them.
    
    In this change, we never remove them, because there are still a number
    of cases where we cannot rewrite to TS enum, so the comments keep
    important information for the users.

---
+++ src/test/java/com/google/javascript/gents/singleTests/enum.js
@@ -0,0 +1,61 @@
+goog.module('an.enum');
+
+/**
+ * A non-trivial comment.
+ * @enum {number}
+ */
+const NumEnum = {
+  A: 0,
+  B: 1
+};
+
+/** @enum {number} */
+const NonConseqNumEnum = {
+  A: 0,
+  B: 2,
+  C: 3,
+  D: -2,
+  E: -1
+};
+
+/** @enum {number} */
+const RepeatedNumEnum = {
+  A: 0,
+  B: 0,
+  C: 2,
+  D: 2
+};
+
+
+/** @enum {string} */
+const StrEnum = {
+  A: 'foo',
+  B: 'bar'
+};
+
+/** @enum {{a: number}} */
+const OtherEnum = {
+  A: {a: 0},
+  B: {a: 1}
+};
+
+class C {}
+
+//!! This cannot be easily converted without structurally changing the code.
+//!! TypeScript does not support inner enums. The enum and the class have to be
+//!! module level siblings, which would lead to having to rewrite all references
+//!! too. Moreover, even checking this is makes the Syntax tests fail, because
+//!! we do not move it to a static declaration.
+//!!
+//!! /** @enum {number} */
+//!! C.InnerEnum = {
+//!!  A: 0,
+//!!  B: 1
+//!! };
+
+exports.C = C;
+exports.NumEnum = NumEnum;
+exports.NonConseqNumEnum = NonConseqNumEnum;
+exports.RepeatedNumEnum = RepeatedNumEnum;
+exports.StrEnum = StrEnum;
+exports.OtherEnum = OtherEnum;
