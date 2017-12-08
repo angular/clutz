cd493ddf8729ad06ac73550bbadd67230ff719be
Evan Martin <martine@danga.com>
2017-12-08 14:50:06 -0800

    emit subclass constructor when we can't see base constructor (#610)
    Clutz attempts to leave out ctors that aren't necessary (e.g.
    accepts same params as the parent class).  If Clutz can't see
    the parent class, then it must emit a ctor because it can't
    know whether the ctor is necessary or not.

--- src/test/java/com/google/javascript/clutz/partial/tte_promise_partial.d.ts
+++ src/test/java/com/google/javascript/clutz/partial/tte_promise_partial.d.ts
@@ -2,6 +2,7 @@ declare namespace ಠ_ಠ.clutz.module$exports$tte$Promise$Partial {
   class PartialDeferred < VALUE = any > extends PartialDeferred_Instance < VALUE > {
   }
   class PartialDeferred_Instance < VALUE = any > extends Base < VALUE > {
+    constructor ( ) ;
     then < RESULT > (opt_onFulfilled ? : ( (a : VALUE ) => PartialDeferred < RESULT > | RESULT ) | null , opt_onRejected ? : ( (a : any ) => any ) | null) : PartialDeferred < RESULT > ;
   }
 }
