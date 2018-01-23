e3d108ab479f09671dff3a8a62bb66ea7e140497
Lucas Sloan <lucas.sloan@gmail.com>
2018-01-23 12:22:11 -0800

    Change DeclarationGeneratorTests and MultifileTests to auto update goldens.
    Passing the environment variable UPDATE_GOLDENS=y when running declaration generators now auto updates all goldens.  _with_platform goldens and goldens with comments are exempted, so as not to mess things up.

--- src/test/java/com/google/javascript/clutz/MultiFileTest.java
+++ src/test/java/com/google/javascript/clutz/MultiFileTest.java
@@ -19,30 +19,30 @@ public class MultiFileTest {
   // Repro for https://github.com/angular/closure-to-dts/issues/101
   @Test
   public void shouldPruneProvidesFromNonrootFile() throws Exception {
-    String expected = DeclarationGeneratorTests.getTestFileText(input("require.d.ts"));
+    File golden = input("require.d.ts");
     assertThatProgram(singletonList(input("require.js")), singletonList(input("provide.js")))
-        .generatesDeclarations(expected);
+        .generatesDeclarations(golden);
   }
 
   @Test
   public void shouldResolveNamedTypes() throws Exception {
-    String expected = DeclarationGeneratorTests.getTestFileText(input("index.d.ts"));
+    File golden = input("index.d.ts");
     assertThatProgram(
             ImmutableList.of(input("index.js"), input("dep.js")), Collections.<File>emptyList())
-        .generatesDeclarations(expected);
+        .generatesDeclarations(golden);
   }
 
   @Test
   public void shouldWorkWithOutOfOrderProvides() throws Exception {
-    String expected = DeclarationGeneratorTests.getTestFileText(input("index.d.ts"));
+    File golden = input("index.d.ts");
     assertThatProgram(
             ImmutableList.of(input("index.js"), input("dep.js")), Collections.<File>emptyList())
-        .generatesDeclarations(expected);
+        .generatesDeclarations(golden);
   }
 
   @Test
   public void googModule() throws Exception {
-    String expected = DeclarationGeneratorTests.getTestFileText(input("goog_module.d.ts"));
+    File golden = input("goog_module.d.ts");
     assertThatProgram(
             ImmutableList.of(
                 input("required_module.js"),
@@ -50,49 +50,49 @@ public class MultiFileTest {
                 input("required.js"),
                 input("goog_module.js")),
             Collections.<File>emptyList())
-        .generatesDeclarations(expected);
+        .generatesDeclarations(golden);
   }
 
   @Test
   public void depgraph() throws Exception {
-    String expected = DeclarationGeneratorTests.getTestFileText(input("depgraph.d.ts"));
+    File golden = input("depgraph.d.ts");
     assertThatProgram(
             ImmutableList.of(input("root.js")),
             ImmutableList.of(
                 input("transitive.js"),
                 input("transitive_unused.js"),
                 input("transitive_namespace.js")))
-        .generatesDeclarations(expected);
+        .generatesDeclarations(golden);
   }
 
   @Test
   public void tsickleEmit() throws Exception {
-    String expected = DeclarationGeneratorTests.getTestFileText(input("tsickle_emit.d.ts"));
+    File golden = input("tsickle_emit.d.ts");
     assertThatProgram(
             ImmutableList.of(input("uses_tsickle_type.js")),
             ImmutableList.of(input("tsickle_emit.skip.tsickle.js")))
-        .generatesDeclarations(expected);
+        .generatesDeclarations(golden);
   }
 
   @Test
   public void skipEmitWithTypedefs() throws Exception {
-    String expected = DeclarationGeneratorTests.getTestFileText(input("skipped.d.ts"));
+    File golden = input("skipped.d.ts");
     assertThatProgram(
             ImmutableList.of(input("uses_type.js")),
             ImmutableList.of(input("skipped_typedef.skip.tsickle.js")))
-        .generatesDeclarations(expected);
+        .generatesDeclarations(golden);
   }
 
   @Test
   public void multifilePartial() throws Exception {
-    String expected = DeclarationGeneratorTests.getTestFileText(input("total.d.ts"));
+    File golden = input("total.d.ts");
     assertThatProgram(
             ImmutableList.of(input("missing_imported_base.js")),
             ImmutableList.of(
                 input("named_base_exporter.js"),
                 input("default_base_exporter.js"),
                 input("default_object_exporter.js")))
-        .generatesDeclarations(expected);
+        .generatesDeclarations(golden);
   }
 
   private File input(String filename) {
--- src/test/java/com/google/javascript/clutz/ProgramSubject.java
+++ src/test/java/com/google/javascript/clutz/ProgramSubject.java
@@ -5,8 +5,10 @@ import static com.google.common.truth.Truth.assert_;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Collections.singletonList;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
+import com.google.common.io.Files;
 import com.google.common.truth.FailureStrategy;
 import com.google.common.truth.StringSubject;
 import com.google.common.truth.Subject;
@@ -14,6 +16,7 @@ import com.google.common.truth.SubjectFactory;
 import com.google.javascript.jscomp.SourceFile;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -73,14 +76,23 @@ class ProgramSubject extends Subject<ProgramSubject, ProgramSubject.Program> {
     super(failureStrategy, subject);
   }
 
-  void generatesDeclarations(String expected) {
+  void generatesDeclarations(File golden) throws IOException {
     String[] parseResult = parse();
     assertThat(parseResult[1]).isEqualTo("");
     String actual = parseResult[0];
     String stripped =
         DeclarationGeneratorTests.GOLDEN_FILE_COMMENTS_REGEXP.matcher(actual).replaceAll("");
-    if (!stripped.equals(expected)) {
-      failureStrategy.failComparing("compilation result doesn't match", expected, stripped);
+    String expected = DeclarationGeneratorTests.getTestFileText(golden);
+    String expectedClean =
+        DeclarationGeneratorTests.GOLDEN_FILE_COMMENTS_REGEXP.matcher(expected).replaceAll("");
+    if (!stripped.equals(expectedClean)) {
+      if (System.getenv("UPDATE_GOLDENS") != null
+          && !golden.getName().endsWith("_with_platform.d.ts")
+          && expected.equals(expectedClean)) {
+        Files.write(stripped, golden, Charsets.UTF_8);
+      } else {
+        failureStrategy.failComparing("compilation result doesn't match", expected, stripped);
+      }
     }
   }
 
