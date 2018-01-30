89e60725389818f0701cef9ade9b634721560209
Rado Kirov <rkirov@gmail.com>
2018-01-30 13:36:48 -0800

    Fix for enum aliasing unknown enums.
    Fixes #669

---
+++ src/test/java/com/google/javascript/clutz/partial/enum_alias.d.ts
@@ -0,0 +1,8 @@
+declare namespace ಠ_ಠ.clutz {
+  type module$exports$enum_alias = ಠ_ಠ.clutz.module$exports$enum_alias ;
+  var module$exports$enum_alias : typeof ಠ_ಠ.clutz.module$exports$enum_alias ;
+}
+declare module 'goog:enum_alias' {
+  import alias = ಠ_ಠ.clutz.module$exports$enum_alias;
+  export default alias;
+}
