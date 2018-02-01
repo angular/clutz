49579bbcf9f40e4e136217cae7f422202d6aead8
Evan Martin <evan.martin@gmail.com>
2018-02-01 13:32:07 -0800

    use whitelists of known global types
    When code refers to e.g. HTMLElement, we want to ensure that we
    emit HTMLElement and not o_o.clutz.HTMLElement.  In incremental
    mode the only thing we have to go on is the "HTMLElement" text
    itself.
    
    This change adds lists of:
    1) all symbols declared by lib.d.ts
    2) all symbols declared in externs that we *don't* want externs for
    
    With those in hand, we can now assume that any symbol that isn't
    declared refers to a global, and we can also avoid emitting externs
    for symbols that TypeScript already declares.
    
    This fixes one important test (which had the wrong reference to "Node")
    and also removes some externs emit for some types that TypeScript
    now covers.

--- src/test/java/com/google/javascript/clutz/partial/missing_platform_externs.js
+++ src/test/java/com/google/javascript/clutz/partial/missing_platform_externs.js
@@ -1,7 +1,6 @@
 goog.module("missing.platform.externs");
 
 //!! Don't need to provide any type for Node, since it's in lib.d.ts
-//!! Note: current emit is wrong, will be fixed in next change.
 /** @return {Node} */
 function foo() { return null }
 
