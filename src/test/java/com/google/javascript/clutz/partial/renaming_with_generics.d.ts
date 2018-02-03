e3738bde6e75507fcf1dfc82b46ac2f5a1afec06
Rado Kirov <rkirov@gmail.com>
2018-02-02 20:49:34 -0800

    Goog.require ambiguity rewriting now works for generic type params. (#676)
    The rewriter logic is now hooked in one more symbol emit code path.

---
+++ src/test/java/com/google/javascript/clutz/partial/renaming_with_generics.d.ts
@@ -0,0 +1,8 @@
+declare namespace ಠ_ಠ.clutz.module$exports$renaming$with$generics {
+  var x : angular.IPromise < module$exports$other$module > | null ;
+  var y : angular.IPromise < module$exports$other$module$destruct.D > | null ;
+}
+declare module 'goog:renaming.with.generics' {
+  import alias = ಠ_ಠ.clutz.module$exports$renaming$with$generics;
+  export = alias;
+}
