69baf43623f74a819a3e464de6350cf2150f7724
Lucas Sloan <lucassloan@google.com>
2018-02-07 11:16:25 -0800

    Refactor the known class aliases code to handle goog modules with class aliases. (#679)
    The goog.module requires a type annotation to work, and the known class aliases map is read in from a file instead of being hard coded.

---
+++ src/test/java/com/google/javascript/clutz/partial/bare_reexport.d.ts
@@ -0,0 +1,8 @@
+declare namespace ಠ_ಠ.clutz {
+  type module$exports$bare$reexport = ಠ_ಠ.clutz.module$exports$original$module.Class ;
+  var module$exports$bare$reexport : typeof ಠ_ಠ.clutz.module$exports$original$module.Class ;
+}
+declare module 'goog:bare.reexport' {
+  import alias = ಠ_ಠ.clutz.module$exports$bare$reexport;
+  export default alias;
+}
