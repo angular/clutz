6931cb0398e138d43893368af862ab664f0ec823
Rado Kirov <rkirov@gmail.com>
2018-02-12 14:49:49 -0800

    Fix interaction between importRewriter and Global aliases. (#684)
    The import rewriter has to be run before the code that looks for global
    aliases. For example:
    
    const EventTarget = goog.require('goog.events.EventTarget');
    
    /** @type {EventTarget} */
    
    The name has to be expanded to goog.events.EventTarget, otherwise we
    would rewrite it as GlobalEventTarget.

--- src/test/java/com/google/javascript/clutz/DeclarationGeneratorTests.java
+++ src/test/java/com/google/javascript/clutz/DeclarationGeneratorTests.java
@@ -80,6 +80,7 @@ public class DeclarationGeneratorTests {
       if (Arrays.asList("partial", "multifilePartial", "partialCrossModuleTypeImports")
           .contains(input.getParentFile().getName())) {
         subject.partialInput = true;
+        subject.knownGoogProvides = ImmutableSet.of("goog.events.EventTarget");
       }
       if (input.getParentFile().getName().equals("partialCrossModuleTypeImports")) {
         subject.knownGoogProvides =
