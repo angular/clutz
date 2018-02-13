2ba05341595ce858a8df14010afd4f1980b1aa25
Lucas Sloan <lucas.sloan@gmail.com>
2018-02-12 16:29:32 -0800

    Improve the typings for goog.Promise.
    Clutz hard codes translations for TTE types, and goog.Promise has 2 type parameters, which wasn't previously accounted for in the translations.
    
    Had to turn on noStrictGenericChecks to make it compile, ts doesn't like extending promise-y things.

---
+++ src/test/java/com/google/javascript/clutz/goog_promise_with_platform.d.ts
@@ -0,0 +1,30 @@
+declare namespace ಠ_ಠ.clutz.goog {
+  class Promise < TYPE , RESOLVER_CONTEXT > extends Promise_Instance < TYPE , RESOLVER_CONTEXT > {
+    static all < TYPE > (promises : any [] ) : ಠ_ಠ.clutz.goog.Promise < TYPE [] , any > ;
+    static race < TYPE > (promises : any [] ) : ಠ_ಠ.clutz.goog.Promise < TYPE , any > ;
+    static resolve < T >(value: ಠ_ಠ.clutz.goog.Promise < T , any > | T): any;
+  }
+  class Promise_Instance < TYPE , RESOLVER_CONTEXT > implements ಠ_ಠ.clutz.goog.Thenable < TYPE > {
+    private noStructuralTyping_: any;
+    constructor (resolver : (a : (a ? : TYPE | PromiseLike < TYPE > | null | { then : any } ) => any , b : (a ? : any ) => any ) => void , opt_context ? : RESOLVER_CONTEXT ) ;
+    then < RESULT > (opt_onFulfilled ? : ( (a : TYPE ) =>  any | RESULT ) | null , opt_onRejected ? : ( (a : any ) => any ) | null) :  any ;
+  }
+}
+declare module 'goog:goog.Promise' {
+  import alias = ಠ_ಠ.clutz.goog.Promise;
+  export default alias;
+}
+declare namespace ಠ_ಠ.clutz.goog {
+  interface Thenable < TYPE > extends PromiseLike < TYPE > {
+    then < RESULT > (opt_onFulfilled ? : ( (a : TYPE ) => ಠ_ಠ.clutz.goog.Thenable < RESULT > | RESULT ) | null , opt_onRejected ? : ( (a : any ) => any ) | null) : ಠ_ಠ.clutz.goog.Thenable < RESULT > ;
+  }
+}
+declare namespace ಠ_ಠ.clutz.goog.Thenable {
+  var IMPLEMENTED_BY_PROP : string ;
+  function addImplementation (ctor : { new ( ...a : any [] ) : ಠ_ಠ.clutz.goog.Thenable < any > } ) : void ;
+  function isImplementedBy (object : any ) : boolean ;
+}
+declare module 'goog:goog.Thenable' {
+  import alias = ಠ_ಠ.clutz.goog.Thenable;
+  export default alias;
+}
