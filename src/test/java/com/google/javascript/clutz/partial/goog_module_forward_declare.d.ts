736f4dcdf185a76345a1f8a3839188de13962775
Lucas Sloan <lucassloan@google.com>
2018-03-22 16:07:22 -0700

    Treat goog.forwardDeclare() as an import for the purposes of building the import rename map.

---
+++ src/test/java/com/google/javascript/clutz/partial/goog_module_forward_declare.d.ts
@@ -0,0 +1,11 @@
+declare namespace ಠ_ಠ.clutz {
+  class module$exports$legacy$namespace$forward$declare extends module$exports$legacy$namespace$forward$declare_Instance {
+  }
+  class module$exports$legacy$namespace$forward$declare_Instance extends ಠ_ಠ.clutz.module$exports$missing$forward$Declare_Instance {
+    constructor ( ) ;
+  }
+}
+declare module 'goog:legacy.namespace.forward.declare' {
+  import alias = ಠ_ಠ.clutz.module$exports$legacy$namespace$forward$declare;
+  export default alias;
+}
