259c76dde00b47cf294ae7ee4d45c004396dfccf
Lucas Sloan <lucassloan@google.com>
2018-03-22 18:48:33 -0700

    Fix an npe in declareLegacyNamespacesAliases(). (#718)

---
+++ src/test/java/com/google/javascript/clutz/partial/declare_legacy_namespace_goog_define.d.ts
@@ -0,0 +1,7 @@
+declare namespace ಠ_ಠ.clutz.legacy.namespace.goog {
+  var define : string ;
+}
+declare module 'goog:legacy.namespace.goog.define' {
+  import alias = ಠ_ಠ.clutz.legacy.namespace.goog.define;
+  export default alias;
+}
