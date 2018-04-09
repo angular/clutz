4294a77f314425fde235a9db604392a0a04b41f1
Rado Kirov <rkirov@gmail.com>
2018-04-09 13:01:17 -0700

    Support new closure nightly. (#726)
    One new class was added to externs and IteratorIterable is inferred
    correctly now.

--- src/test/java/com/google/javascript/clutz/generator_with_platform.d.ts
+++ src/test/java/com/google/javascript/clutz/generator_with_platform.d.ts
@@ -1,6 +1,5 @@
 declare namespace ಠ_ಠ.clutz.module$exports$generators_total {
-  //!! AFAICT, closure just infers ? and we faithfully translate that to 'any'.
-  function inferredG ( ) : any ;
+  function inferredG ( ) : IterableIterator < any > ;
   function templateG < T > (iter : Iterator < T > ) : IterableIterator < T > ;
   function typedG ( ) : IterableIterator < number > ;
 }
