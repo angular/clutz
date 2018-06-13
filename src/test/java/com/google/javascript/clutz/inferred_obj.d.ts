ae69970dbae3aa6d1e7ee418e616fcdc5c5ebf7d
Rado Kirov <rkirov@gmail.com>
2018-06-12 18:03:44 -0700

    Fix test broken with latest closure head.
    It appears a number of invalid property names are simply no longer
    reported seen by clutz, because the undelying closure object has
    pre-filtered them.
    
    This means we no longer print '//!! clutz skipped property foo' type of
    comments on them.

--- src/test/java/com/google/javascript/clutz/inferred_obj.d.ts
+++ src/test/java/com/google/javascript/clutz/inferred_obj.d.ts
@@ -1,6 +1,4 @@
 declare namespace ಠ_ಠ.clutz.inferred.nested.obj {
-  // skipping property '0123' because it is not a valid symbol.
-  // skipping property 'must-be-quoted' because it is not a valid symbol.
   var nestedObj : { '0123' : number , 'must-be-quoted' : number , quoted : number , regular : number } ;
   var quotedProp : number ;
   var regular : number ;
