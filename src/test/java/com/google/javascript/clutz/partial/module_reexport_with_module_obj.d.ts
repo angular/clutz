08fe98211c71917d377f9a0212b751dfaa08e1af
Rado Kirov <rkirov@gmail.com>
2018-08-30 14:28:37 -0700

    Support aliases through module objects.
    Now clutz understands the pattern:
    
    ```
    goog.module('bare.named.reexport');
    
    const Mod = goog.require('original.module');
    
    exports.x = Mod.x;
    ```
    
    and generates proper reexports. By necessity of incremental mode it
    generates a type and var reexport of x, even if it is only a value or
    only a type.
    
    Also, use consistently getSecondChild() when accessing Closure AST.

---
+++ src/test/java/com/google/javascript/clutz/partial/module_reexport_with_module_obj.d.ts
@@ -0,0 +1,10 @@
+declare namespace ಠ_ಠ.clutz.module$exports$bare$named$reexport {
+  type Class = ಠ_ಠ.clutz.module$exports$original$module.Class ;
+  var Class : typeof ಠ_ಠ.clutz.module$exports$original$module.Class ;
+  type fn = ಠ_ಠ.clutz.module$exports$original$module.fn ;
+  var fn : typeof ಠ_ಠ.clutz.module$exports$original$module.fn ;
+}
+declare module 'goog:bare.named.reexport' {
+  import reexport = ಠ_ಠ.clutz.module$exports$bare$named$reexport;
+  export = reexport;
+}
