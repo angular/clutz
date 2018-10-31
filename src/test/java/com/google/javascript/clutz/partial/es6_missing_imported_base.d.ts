b7961b6fea8605b89bc961cc6f0301e41db53046
Evan Martin <evan.martin@gmail.com>
2018-10-31 14:35:00 -0700

    update tests to new closure upstream
    This change restores a green build by fixing one bug and checking in new
    baselines for the failing tests.
    
    I added a file comment to base.js to show what it's for, and added a
    newly required annotation on it.

--- src/test/java/com/google/javascript/clutz/partial/es6_missing_imported_base.d.ts
+++ src/test/java/com/google/javascript/clutz/partial/es6_missing_imported_base.d.ts
@@ -2,22 +2,22 @@ declare namespace ಠ_ಠ.clutz.module$exports$missing$imported$base {
   class ClassExtendingDefaultObjectExporterBaseClass extends ClassExtendingDefaultObjectExporterBaseClass_Instance {
   }
   class ClassExtendingDefaultObjectExporterBaseClass_Instance extends ಠ_ಠ.clutz.module$exports$default$object$exporter.BaseClass_Instance {
-    constructor ( ...var_args : any [] ) ;
+    constructor ( ...a : any [] ) ;
   }
   class ClassExtendingMissingDestructuredRequire extends ClassExtendingMissingDestructuredRequire_Instance {
   }
   class ClassExtendingMissingDestructuredRequire_Instance extends ಠ_ಠ.clutz.module$exports$named$base$exporter.MissingDestructuredRequire_Instance {
-    constructor ( ...var_args : any [] ) ;
+    constructor ( ...a : any [] ) ;
   }
   class ClassExtendingMissingRequire extends ClassExtendingMissingRequire_Instance {
   }
   class ClassExtendingMissingRequire_Instance extends ಠ_ಠ.clutz.module$exports$default$base$exporter_Instance {
-    constructor ( ...var_args : any [] ) ;
+    constructor ( ...a : any [] ) ;
   }
   class ClassExtendingRenamedDestructuredRequire extends ClassExtendingRenamedDestructuredRequire_Instance {
   }
   class ClassExtendingRenamedDestructuredRequire_Instance extends ಠ_ಠ.clutz.module$exports$named$base$exporter.OriginalName_Instance {
-    constructor ( ...var_args : any [] ) ;
+    constructor ( ...a : any [] ) ;
   }
   var DeclarationOfMissingRequire : ಠ_ಠ.clutz.module$exports$default$base$exporter | null ;
   function FuncWithMissingRequireParam (c : ಠ_ಠ.clutz.module$exports$default$base$exporter | null ) : void ;
