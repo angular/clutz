2072e0ff01d4064319a707bbbc298bd051d2964b
teppeis <teppeis@gmail.com>
2018-12-05 23:50:21 +0900

    Add tests for --closure_env

---
+++ src/test/java/com/google/javascript/clutz/ClosureEnvTest.java
@@ -0,0 +1,57 @@
+package com.google.javascript.clutz;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.file.Path;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ClosureEnvTest {
+
+  private final Path inputFile =
+      DeclarationGeneratorTests.getTestInputFile("env" + File.separatorChar + "browser.js");
+  private PrintStream defaultOut;
+
+  @Before
+  public void setUp() {
+    defaultOut = System.out;
+    System.setOut(nullPrintStream);
+  }
+
+  @After
+  public void tearDown() {
+    System.setOut(defaultOut);
+  }
+
+  private final PrintStream nullPrintStream =
+      new PrintStream(
+          new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {}
+          });
+
+  @Test
+  public void shouldHaveErrorForNoExterns() throws Exception {
+    Options opts = new Options(new String[] {inputFile.toString(), "--debug"});
+    final DeclarationGenerator generator = new DeclarationGenerator(opts);
+    generator.generateDeclarations();
+    assertThat(generator.hasErrors()).isTrue();
+  }
+
+  @Test
+  public void shouldHaveNoErrorsForBrowserExterns() throws Exception {
+    Options opts =
+        new Options(new String[] {inputFile.toString(), "--debug", "--closure_env", "BROWSER"});
+    final DeclarationGenerator generator = new DeclarationGenerator(opts);
+    generator.generateDeclarations();
+    assertThat(generator.hasErrors()).isFalse();
+  }
+}
