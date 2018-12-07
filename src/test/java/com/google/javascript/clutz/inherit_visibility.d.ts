3e2de373455fd067e08a2dae06d56c29b8c8bab8
teppeis <teppeis@gmail.com>
2018-12-07 17:19:02 +0900

    Inherit visibility of parent properties

---
+++ src/test/java/com/google/javascript/clutz/inherit_visibility.d.ts
@@ -0,0 +1,27 @@
+declare namespace ಠ_ಠ.clutz.module$exports$inherit_visibility {
+  class Child extends ಠ_ಠ.clutz.module$exports$inherit_visibility.Parent {
+    private noStructuralTyping_module$exports$inherit_visibility_Child : any;
+    protected protected ( ) : void ;
+    protected protectedNoJSDoc ( ) : void ;
+    publicExplicit ( ) : void ;
+    publicImplicit ( ) : void ;
+  }
+  class GrandChild extends ಠ_ಠ.clutz.module$exports$inherit_visibility.Child {
+    private noStructuralTyping_module$exports$inherit_visibility_GrandChild : any;
+    protected protected ( ) : void ;
+    protected protectedNoJSDoc ( ) : void ;
+    publicExplicit ( ) : void ;
+    publicImplicit ( ) : void ;
+  }
+  class Parent {
+    private noStructuralTyping_module$exports$inherit_visibility_Parent : any;
+    protected protected ( ) : void ;
+    protected protectedNoJSDoc ( ) : void ;
+    publicExplicit ( ) : void ;
+    publicImplicit ( ) : void ;
+  }
+}
+declare module 'goog:inherit_visibility' {
+  import inherit_visibility = ಠ_ಠ.clutz.module$exports$inherit_visibility;
+  export = inherit_visibility;
+}
