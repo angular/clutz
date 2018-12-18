8b1abaefd07285039dbe7aa4ee8e4a482260cab8
Rado Kirov <rkirov@gmail.com>
2018-12-18 05:11:11 +0000

    Adds a new typedef TexImageSource to CLOSURE_EXTERNS_NOT_USED_IN_TYPESCRIPT list.
    As usual, it is easier to not emit any types for these new platform
    APIs, until someone needs them from TypeScript and we can verify there
    is no matching lib.d.ts version for them.

--- src/main/java/com/google/javascript/clutz/PlatformSymbols.java
+++ src/main/java/com/google/javascript/clutz/PlatformSymbols.java
@@ -294,6 +294,7 @@ public class PlatformSymbols {
           "StorageInfo",
           "StorageQuota",
           "TaskAttributionTiming",
+          "TexImageSource",
           "TextDecoder",
           "TextEncoder",
           "TextRange",
