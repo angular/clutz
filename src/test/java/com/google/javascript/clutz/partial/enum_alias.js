7acfed86969a37074d66233ebabbe296dd99bb72
Rado Kirov <rkirov@gmail.com>
2019-03-11 16:20:04 -0700

    Turn off enum_alias test by removing enum annotation. (#863)
    The current emit is wrong - self circular. Closure team wants to land a
    change that changes it into another (also wrong emit), turn off test to
    unblock that.
    
    https://github.com/angular/clutz/issues/862 to track proper fix.

--- src/test/java/com/google/javascript/clutz/partial/enum_alias.js
+++ src/test/java/com/google/javascript/clutz/partial/enum_alias.js
@@ -2,7 +2,9 @@ goog.module('enum_alias');
 
 const OtherEnum = goog.require('not_visible');
 
-/** @enum {string} */
+//!! This test is temporary turned off to enable
+//!! change in behavior on closure side.
+//!! /** @enum {string} */
 const Enum = OtherEnum;
 
 exports = Enum;
