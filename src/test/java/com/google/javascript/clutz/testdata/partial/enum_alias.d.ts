1650ab4a6bd074e587bbdc9ca48553aca48b0505
Rado Kirov <rkirov@gmail.com>
2019-08-21 17:11:58 -0700

    Revert back the enum alias test.
    The behavior changed with a closure change in March. The test is still
    broken but in a different way.

--- src/test/java/com/google/javascript/clutz/testdata/partial/enum_alias.d.ts
+++ src/test/java/com/google/javascript/clutz/testdata/partial/enum_alias.d.ts
@@ -1,12 +1,8 @@
 // Generated from src/test/java/com/google/javascript/clutz/testdata/partial/enum_alias.js
 declare namespace ಠ_ಠ.clutz {
-  //!! Closure will begin emitting this soon:
-  //!! enum module$exports$enum_alias { }
-  //!!
-  //!! That is incorrect, wait until closure starts emitting it
-  //!! at head and then reenable the test and fix properly.
-  //!! https://github.com/angular/clutz/issues/862
-  let module$exports$enum_alias : any ;
+  //!! This is broken. See https://github.com/angular/clutz/issues/862.
+  enum module$exports$enum_alias {
+  }
 }
 declare module 'goog:enum_alias' {
   import enum_alias = ಠ_ಠ.clutz.module$exports$enum_alias;
