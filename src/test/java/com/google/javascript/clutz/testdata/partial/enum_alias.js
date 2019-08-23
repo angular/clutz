f7f17b72361b35fe09970b77387e314fdf29e44c
Rado Kirov <rkirov@gmail.com>
2019-08-23 16:35:29 -0700

    Revert back the enum alias test. (#911)
    The behavior changed with a closure change in March. The test is still
    broken but in a different way.

--- src/test/java/com/google/javascript/clutz/testdata/partial/enum_alias.js
+++ src/test/java/com/google/javascript/clutz/testdata/partial/enum_alias.js
@@ -2,9 +2,7 @@ goog.module('enum_alias');
 
 const OtherEnum = goog.require('not_visible');
 
-//!! This test is temporary turned off to enable
-//!! change in behavior on closure side.
-//!! /** @enum {string} */
+/** @enum {string} */
 const Enum = OtherEnum;
 
 exports = Enum;
