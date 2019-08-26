733982c9a6e09a2e13f914b6e53f9ec037161792
Rado Kirov <rkirov@gmail.com>
2019-08-26 13:48:54 -0700

    Fix exports of local aliases of the form `exports.B = exports.A`. (#912)
    It appears that the emit was previously very broken for this case
    (syntactically incorrect). This change simply skips the alias rewriting
    when "... = exports...." pattern is detected syntactically. For the rest
    we still assume the syntactic rewriter is desirable, like the case
    `exports.A = externalModule.Foo`.

---
+++ src/test/java/com/google/javascript/clutz/testdata/export_reexport.d.ts
@@ -0,0 +1,12 @@
+// Generated from src/test/java/com/google/javascript/clutz/testdata/export_reexport.js
+declare namespace ಠ_ಠ.clutz.module$exports$exports$reexport {
+  let A : string ;
+  let TypedExportLocal : string ;
+  let TypedReexport : string ;
+  let UntypedExportLocal : string ;
+  let UntypedReexport : string ;
+}
+declare module 'goog:exports.reexport' {
+  import reexport = ಠ_ಠ.clutz.module$exports$exports$reexport;
+  export = reexport;
+}
---
+++ src/test/java/com/google/javascript/clutz/testdata/export_reexport.js
@@ -0,0 +1,17 @@
+goog.module('exports.reexport');
+
+/** @type {string} */
+const LOCAL = '';
+
+/** @type {string} */
+exports.A = '';
+
+/** @type {string} */
+exports.TypedExportLocal = LOCAL;
+
+exports.UntypedExportLocal = LOCAL;
+
+/** @type {string} */
+exports.TypedReexport = exports.A;
+
+exports.UntypedReexport = exports.A;
