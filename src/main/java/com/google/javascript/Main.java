15ffae28b7e414acca74a6c9fbb129c3d700500a
Dominic Kramer <dominicdkramer@google.com>
2019-09-04 14:53:15 -0700

    reformat code

--- src/main/java/com/google/javascript/Main.java
+++ src/main/java/com/google/javascript/Main.java
@@ -4,20 +4,21 @@ import com.google.javascript.clutz.DeclarationGenerator;
 import com.google.javascript.gents.TypeScriptGenerator;
 
 public class Main {
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println(String.format("Usage: %s <clutz|gents> [...args]", Main.class.getSimpleName()));
-            System.exit(1);
-        }
-
-        String tool = args[0];
-        String[] subArgs = new String[args.length-1];
-        System.arraycopy(args, 1, subArgs, 0, subArgs.length);
-
-        if ("clutz".equals(tool)) {
-            DeclarationGenerator.main(subArgs);
-        } else if ("gents".equals(tool)) {
-            TypeScriptGenerator.main(subArgs);
-        }
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println(
+          String.format("Usage: %s <clutz|gents> [...args]", Main.class.getSimpleName()));
+      System.exit(1);
     }
+
+    String tool = args[0];
+    String[] subArgs = new String[args.length - 1];
+    System.arraycopy(args, 1, subArgs, 0, subArgs.length);
+
+    if ("clutz".equals(tool)) {
+      DeclarationGenerator.main(subArgs);
+    } else if ("gents".equals(tool)) {
+      TypeScriptGenerator.main(subArgs);
+    }
+  }
 }
