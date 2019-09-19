7ba603f85200b3c341c68474011b32220be406da
Laura Harker <lharker@google.com>
2019-09-18 18:27:25 -0700

    Replace call to goog.define with assignment to the result of goog.define

--- src/test/java/com/google/javascript/clutz/testdata/partial/declare_legacy_namespace_goog_define.js
+++ src/test/java/com/google/javascript/clutz/testdata/partial/declare_legacy_namespace_goog_define.js
@@ -2,7 +2,7 @@ goog.module('legacy.namespace.goog.define');
 goog.module.declareLegacyNamespace();
 
 /** @define {string} */
-goog.define('GOOG_DEFINE', 'goog.define');
+const GOOG_DEFINE = goog.define('GOOG_DEFINE', 'goog.define');
 
 //!!TODO actually emit the legacy namespace alias for the export
 exports = GOOG_DEFINE;
