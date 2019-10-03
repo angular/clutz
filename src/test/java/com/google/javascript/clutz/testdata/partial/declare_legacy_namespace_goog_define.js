b69b263f7889d4f6517dba3383f8721617a61fb8
Evan Martin <evan.martin@gmail.com>
2019-10-03 14:16:52 -0700

    update test to reflect new requirement of goog.define
    The previous code now fails with the new error
      JSC_DEFINE_CALL_WITHOUT_ASSIGNMENT

--- src/test/java/com/google/javascript/clutz/testdata/partial/declare_legacy_namespace_goog_define.js
+++ src/test/java/com/google/javascript/clutz/testdata/partial/declare_legacy_namespace_goog_define.js
@@ -2,7 +2,7 @@ goog.module('legacy.namespace.goog.define');
 goog.module.declareLegacyNamespace();
 
 /** @define {string} */
-goog.define('GOOG_DEFINE', 'goog.define');
+const def = goog.define('GOOG_DEFINE', 'goog.define');
 
 //!!TODO actually emit the legacy namespace alias for the export
-exports = GOOG_DEFINE;
+exports = def;
