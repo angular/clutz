483880f7ffdd8952b9b66de049bbb7b98069952d
Laura Harker <lharker@google.com>
2019-10-07 14:15:21 -0700

    Rename goog.provide from 'global' to 'globalNs'
    A Closure Compiler change will begin special casing 'global' as it is
    a built-in in Node environments. See Google internal
    http://cl/270391864.

--- src/test/java/com/google/javascript/clutz/testdata/partial/global_generic_type.d.ts
+++ src/test/java/com/google/javascript/clutz/testdata/partial/global_generic_type.d.ts
@@ -1,17 +1,17 @@
 // Generated from src/test/java/com/google/javascript/clutz/testdata/partial/global_generic_type.js
-declare namespace ಠ_ಠ.clutz.global.generic {
+declare namespace ಠ_ಠ.clutz.globalNs.generic {
   let type : Map < string , string > ;
 }
-declare module 'goog:global.generic.type' {
-  import type = ಠ_ಠ.clutz.global.generic.type;
+declare module 'goog:globalNs.generic.type' {
+  import type = ಠ_ಠ.clutz.globalNs.generic.type;
   export default type;
 }
 // Generated from src/test/java/com/google/javascript/clutz/testdata/partial/global_generic_type.js
-declare namespace ಠ_ಠ.clutz.global.non.generic {
+declare namespace ಠ_ಠ.clutz.globalNs.non.generic {
   let type : Map ;
 }
-declare module 'goog:global.non.generic.type' {
-  import type = ಠ_ಠ.clutz.global.non.generic.type;
+declare module 'goog:globalNs.non.generic.type' {
+  import type = ಠ_ಠ.clutz.globalNs.non.generic.type;
   export default type;
 }
 // Generated from src/test/java/com/google/javascript/clutz/testdata/partial/global_generic_type.js
