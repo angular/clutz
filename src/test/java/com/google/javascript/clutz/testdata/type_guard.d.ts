eeaa7d86fdf69ce122703e5746bb982daad279c4
Rado Kirov <rkirov@gmail.com>
2019-10-08 15:11:20 -0700

    Removes the type_guard.* test.
    Previously this test was using the goog.isDef which was deprecated in
    favor of ' !== undefined'. However, since we run this test without
    externs, it is currently broken because 'undefined' is *ahem* not
    defined to Closure.
    
    Instead, of fixing it yet once again, I noticed that this test doesn't
    test anything significant, because the resulting .d.ts doesn't depend on
    the function implemenation. So this time I am just deleting it.

--- src/test/java/com/google/javascript/clutz/testdata/type_guard.d.ts
+++
@@ -1,9 +0,0 @@
-// Generated from src/test/java/com/google/javascript/clutz/testdata/type_guard.js
-declare namespace ಠ_ಠ.clutz.a {
-  function b (opt_precision ? : number ) : string ;
-  function c (s : number ) : string ;
-}
-declare module 'goog:a' {
-  import a = ಠ_ಠ.clutz.a;
-  export = a;
-}
