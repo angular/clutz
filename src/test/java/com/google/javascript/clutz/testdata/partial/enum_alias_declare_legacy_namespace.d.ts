c7a4c49a102e5e1ba38565fcb4d444d0d4b0c331
nickreid <nickreid@google.com>
2019-12-13 11:58:29 -0800

    Correctly output common enum export style in clutz.
    ```
    goog.module('foo.bar');
    goog.module.declareLegacyNamespace();
    
    const Enum = {...};
    exports.Enum = Enum;
    ```
    
    The change adds unnecessary empty `declare namespace` in enum_alias_declare_legacy_namespace test but it shouldn't affect anything. Ideally for that test the namespace will contain proper alias pointing to goog.require, but it's a bigger issue and separate bug.
    
    Tested:
      unit tests
    PiperOrigin-RevId: 285443960

--- src/test/java/com/google/javascript/clutz/testdata/partial/enum_alias_declare_legacy_namespace.d.ts
+++ src/test/java/com/google/javascript/clutz/testdata/partial/enum_alias_declare_legacy_namespace.d.ts
@@ -7,11 +7,6 @@ declare module 'goog:enum_alias2' {
   import enum_alias2 = ಠ_ಠ.clutz.enum_alias2;
   export = enum_alias2;
 }
-//!! This is incorrect as namespace should contain module$contents$enum_alias2_Enum
-//!! alias to notVisible.OtherEnum
-// Generated from src/test/java/com/google/javascript/clutz/testdata/partial/enum_alias_declare_legacy_namespace.js
-declare namespace ಠ_ಠ.clutz {
-}
 // Generated from src/test/java/com/google/javascript/clutz/testdata/partial/enum_alias_declare_legacy_namespace.js
 declare namespace ಠ_ಠ.clutz.module$exports$enum_alias2 {
   export import Enum = ಠ_ಠ.clutz.module$contents$enum_alias2_Enum ;
