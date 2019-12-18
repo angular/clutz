778494ce499cfe712f3212b1bddd45c32181cd7c
nickreid <nickreid@google.com>
2019-12-17 17:16:13 -0800

    Apply `JSTypeResolver` to resolve all types during compilation.
    All types are now captured by the resolver and guaranteed to be resolved at known time-points during compilation.
    
    PiperOrigin-RevId: 286093481

--- src/test/java/com/google/javascript/clutz/testdata/partial/aliased_template_argument.d.ts
+++ src/test/java/com/google/javascript/clutz/testdata/partial/aliased_template_argument.d.ts
@@ -3,7 +3,7 @@ declare namespace ಠ_ಠ.clutz {
   class module$exports$aliased$template$argument {
     private noStructuralTyping_module$exports$aliased$template$argument : any;
     methodWithBareArg ( ) : ಠ_ಠ.clutz.module$exports$aliased$template$argument.typedef ;
-    methodWithTemplateArg ( ) : ಠ_ಠ.clutz.unknown.extern.type < ಠ_ಠ.clutz.module$contents$aliased$template$argument_aliasedtypedef > ;
+    methodWithTemplateArg ( ) : ಠ_ಠ.clutz.unknown.extern.type < ಠ_ಠ.clutz.module$exports$aliased$template$argument.typedef > ;
   }
 }
 // Generated from src/test/java/com/google/javascript/clutz/testdata/partial/aliased_template_argument.js
@@ -14,8 +14,4 @@ declare namespace ಠ_ಠ.clutz.module$exports$aliased$template$argument {
 declare module 'goog:aliased.template.argument' {
   import argument = ಠ_ಠ.clutz.module$exports$aliased$template$argument;
   export default argument;
-}
-// Generated from src/test/java/com/google/javascript/clutz/testdata/partial/aliased_template_argument.js
-declare namespace ಠ_ಠ.clutz {
-  type module$contents$aliased$template$argument_aliasedtypedef = { field ? : number } ;
 }
--- src/test/java/com/google/javascript/clutz/testdata/partial/aliased_template_argument.js
+++ src/test/java/com/google/javascript/clutz/testdata/partial/aliased_template_argument.js
@@ -1,9 +1,3 @@
-//!! Closure somehow doesn't properly collapse Class.typedef, aliasedtypedef, and
-//!! aliasedtypedef the template type argument.  Make sure to emit types for both
-//!! Class.typedef (module$exports$aliased$template$argument.typedef) and
-//!! aliasedtypedef (module$contents$aliased$template$argument_aliasedtypedef),
-//!! so that emitting the name of aliasedtypedef the template type argument corresponds
-//!! to a defined type
 goog.module('aliased.template.argument');
 
 var Class = class {
