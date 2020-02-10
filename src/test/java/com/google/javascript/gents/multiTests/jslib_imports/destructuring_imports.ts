d617bcff7b72759892fdb6cc511949cb1a46cefe
Evan Martin <evanm@google.com>
2020-02-10 01:45:29 -0800

    delete gents tests for destructuring imports
    We fixed this in google3 by fixing the JS separately.
    
    PiperOrigin-RevId: 294172458

--- src/test/java/com/google/javascript/gents/multiTests/jslib_imports/destructuring_imports.ts
+++
@@ -1,8 +0,0 @@
-import {num} from 'goog:module.exp.nodef';
-let n = num;
-import X from 'goog:module.exp.both';
-import {num2} from 'goog:module.exp.both';
-X();
-n += num2;
-import * as already_converted_to_ts_keep_module from '../converts_ts_module_require/already_converted_to_ts_keep';
-const {foo, bar} = already_converted_to_ts_keep_module;
--- src/test/java/com/google/javascript/gents/multiTests/jslib_imports/destructuring_imports_same_module.ts
+++
@@ -1,2 +0,0 @@
-import * as already_converted_to_ts_keep_module from '../converts_ts_module_require/already_converted_to_ts_keep';
-const {foo, bar} = already_converted_to_ts_keep_module;
