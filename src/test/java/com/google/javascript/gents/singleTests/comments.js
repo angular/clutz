0b29e3e00a5db36c2b71490e50c2fbc9247f02a1
Dominic Kramer <dominicdkramer@google.com>
2020-03-06 10:34:57 -0800

    The 'comments' tests have been updated to reflect the current (incorrect) state of how gents processes comments.
    However, having the tests in this way clearly illustrates how
    gents processes comments so that 'comments.ts' can be updated
    to the correct expected output to facilitate addressing the
    syntactic mangling issues as needed.
    
    PiperOrigin-RevId: 299385983

--- src/test/java/com/google/javascript/gents/singleTests/comments.js
+++ src/test/java/com/google/javascript/gents/singleTests/comments.js
@@ -111,4 +111,105 @@ a(
 c();
 // comment in the end
 
+Promise.resolve().then(/** before `() =>` but after `.then(` */ () => {
+  a();
+});
+
+Promise.resolve().then(() => 'a')
+                 .then(() => 'b'); // on the same line as `.then(() => 'b');`
+
+if (true) {
+  if (true) {
+    if (true) {
+    }   //  on the same line as innermost }
+  }     //  on the same line as the middle }
+}       //  on the same line as the outermost }
+
+// ob1 comment
+const ob1 = {
+  a: 'a', // on the same line as `a: 'a'`
+  b: 'b' /* on the same line as `b: 'b'` */,
+  // on the line after `b: 'b'`
+  c: 'c', // on the same line as `c: 'c'`
+  // on the line after `c: 'c'`
+  d: [
+    1, // one the same line as `1,`
+    2,
+    3, // one the same line as `3,`
+       // on the line after `3,`
+  ],
+  e: new RegExp(
+      'pattern',
+      'g' // on the same line as `'g'` in the RegExp constructor
+  )
+};
+
+// object with trailing comma
+const ob2 = {
+  a: 'a',
+  b: 'b',
+};
+
+// object without trailing comma
+const ob3 = {
+  a: 'a',
+  b: 'b'
+};
+
+// array with trailing comma
+const arr1 = [
+  1,
+  2,
+];
+
+// array without trailing comma
+const arr2 = [
+  1,
+  2
+];
+
+// handle unicode
+const bread = 'חנות ממתקים';
+
+// handle various number formats
+const num1 = 1000000;
+const num2 = 1e10;
+const num3 = 0.0000001;
+const num4 = 1e-15;
+const num5 = 0o51;
+const num6 = 0b11010;
+const num7 = 0xaBc;
+
+function f1() {
+} // no blank line separating
+function f2() {
+} // no blank line separating
+let x1; // no blank line separating
+let x2;
+
+function f3() {
+} // one blank line separating
+
+function f4() {
+} // two blank lines separating
+
+
+let x3; // blank lines separating
+
+
+
+let x4;
+
+let p;// no space before comment
+let q; // one space before comment
+let r;  // two spaces before comment
+
+// eventual test to ensure
+//   if(){...}else if (...){...}
+// does not get translated to
+//   if(){...}else{if(...){...}}
+if (true) {
+} else if (true) {
+}
+
 exports = {A, X, B};
