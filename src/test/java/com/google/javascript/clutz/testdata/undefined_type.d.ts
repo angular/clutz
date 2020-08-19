453336d1d17a2c22876f4b9cdf1aac64f77bddc7
TSJS team <no-reply@google.com>
2020-08-19 05:09:14 -0700

    Add test cases for undefined-returning functions as arguments.
    The new test cases will make clear the result of using `undefined` in Closure
    JS functions from TypeScript and be used to evaluate future changes.
    
    PiperOrigin-RevId: 327413637

--- src/test/java/com/google/javascript/clutz/testdata/undefined_type.d.ts
+++ src/test/java/com/google/javascript/clutz/testdata/undefined_type.d.ts
@@ -1,17 +1,116 @@
 // Generated from src/test/java/com/google/javascript/clutz/testdata/undefined_type.js
 declare namespace ಠ_ಠ.clutz.undefinedns {
-  class C {
-    private noStructuralTyping_undefinedns_C : any;
-    constructor (f : ( ) => void , g : ( ) => string | undefined ) ;
-  }
-  let a : undefined ;
-  type alias = { foo ? : boolean } ;
-  let b : undefined ;
-  let c : undefined | string ;
-  function f (a : undefined , b : undefined | string , c ? : string ) : void ;
-  function g ( ) : void ;
-  function h ( ) : void ;
-  function i ( ) : undefined | string ;
+  /**
+   * undefined as the return type of a function alias becomes void.  Void is
+   * allowed because the return type is never used.
+   */
+  type FuncReturnsUndefAlias = ( ) => void ;
+  /**
+   * undefined in a union type returned by a function alias remains undefined to
+   * avoid void values, but means some uses of aliases will need to explicitly
+   * return undefined in TypeScript.
+   */
+  type FuncReturnsUndefUnionAlias = ( ) => number | undefined ;
+  /**
+   * A type alias for a union including undefined remains undefined.
+   */
+  type UndefUnionAlias = boolean | undefined ;
+  /**
+   * Fields using undefined or void become optional fields.  The nullable field
+   * remains a union with null.
+   */
+  type UndefUnionRecord = { bar ? : boolean , baz : boolean | null , foo ? : boolean } ;
+  /**
+   * A return type that includes undefined remains undefined in TS instead of
+   * becoming void, because the return value of this function may be assigned to
+   * a value.
+   */
+  function funcReturnsUndefUnion ( ) : undefined | string ;
+  /**
+   * A function that returns undefined returns void in TypeScript.
+   *
+   * This is safe because the return value is unused.
+   */
+  function funcReturnsUndefined ( ) : void ;
+  /**
+   * A function that returns void returns void in TypeScript.
+   *
+   * Similar to funcReturnsUndefined above.
+   */
+  function funcReturnsVoid ( ) : void ;
+  /**
+   * A function parameter that matches a predefined alias will be emitted as
+   * that alias, meaning it will still return undefined.
+   */
+  function matchesFuncAlias (funcAlias : ಠ_ಠ.clutz.undefinedns.FuncReturnsUndefUnionAlias ) : void ;
+  /**
+   * A function accepting and returning a union type including undefined will
+   * still return undefined in TypeScript.
+   *
+   * This is to avoid void values in TypeScript code.
+   */
+  function returnsUndefUnionAlias (union : boolean | undefined ) : boolean | undefined ;
+  /**
+   * A function parameter that returns a type alias that includes undefined will
+   * still return undefined in TypeScript.
+   *
+   * This requires passing a function that explicitly returns undefined, but
+   * since there's no way to know how the alias is used otherwise, it's not safe
+   * to emit void.
+   */
+  function takesUndefAliasFunc (undefAliasFunc : ( ) => boolean | undefined ) : void ;
+  /**
+   * A function parameter that returns undefined returns void in TypeScript.
+   *
+   * This is safe because the return value is unused. Note that this matches
+   * FuncReturnsUndefAlias, so that alias is used for the type in TypeScript.
+   */
+  function takesUndefFunc (undefFunc : ಠ_ಠ.clutz.undefinedns.FuncReturnsUndefAlias ) : void ;
+  /**
+   * A record parameter with a function that returns undefined returns void in
+   * TypeScript.
+   *
+   * This is safe for similar reasons to the above.  Note that this matches the
+   * FuncReturnsUndefAlias, so that alias is used in TypeScript.
+   */
+  function takesUndefFuncRecord (undefFuncRecord : { func : ಠ_ಠ.clutz.undefinedns.FuncReturnsUndefAlias } ) : void ;
+  /**
+   * A function that returns a union type including undefined as a parameter
+   * returns undefined in TypeScript.
+   *
+   * Clutz will only emit void for functions that only return undefined, to
+   * avoid having void value types.
+   *
+   * TODO(b/162851479): Allow void here since this function is invoked from
+   * JavaScript, where the void return type is treated as undefined.
+   */
+  function takesUndefUnionFunc (undefUnionFunc : ( ) => string | undefined ) : void ;
+  /**
+   * A function parameter that returns void returns void in TypeScript.
+   *
+   * This allows passing functions that omit a return value, and is safe because
+   * the return value of the parameter function is not used.
+   */
+  function takesVoidFunc (voidFunc : ಠ_ಠ.clutz.undefinedns.FuncReturnsUndefAlias ) : void ;
+  /**
+   * Omitting the return type of a function causes it to return void in TS.
+   */
+  function undefParamsNoReturn (a : undefined , b : undefined | string , c ? : string ) : void ;
+  /**
+   * Undefined values in JS become undefined in TypeScript.
+   */
+  let undefProp : undefined ;
+  /**
+   * Undefined in a union value remains undefined.
+   */
+  let undefUnionProp : undefined | string ;
+  /**
+   * Void values in JS become undefined in TypeScript.
+   *
+   * Void in JS is a synonym for undefined, but is a distinct type in TypeScript
+   * that's not interchangeable with undefined values.
+   */
+  let voidProp : undefined ;
 }
 // Generated from src/test/java/com/google/javascript/clutz/testdata/undefined_type.js
 declare module 'goog:undefinedns' {
--- src/test/java/com/google/javascript/clutz/testdata/undefined_type.js
+++ src/test/java/com/google/javascript/clutz/testdata/undefined_type.js
@@ -1,52 +1,189 @@
+/**
+ * @fileoverview Test cases for undefined and void.
+ *
+ * In Closure JavaScript, void and undefined are interchangeable, but in
+ * TypeScript they are not.  In TypeScript, undefined is assignable to void, but
+ * not vice-versa.  Functions that omit a return in TypeScript return void, not
+ * undefined.  A function typed as returning undefined must have an explicit
+ * return statement.
+ *
+ * We want to allow omitting return statements where possible, but we also
+ * want to avoid allowing void in value types in TypeScript because of the
+ * asymmetry with undefined.
+ */
+
 goog.provide('undefinedns');
 
 /**
- * @const {undefined}
+ * undefined as the return type of a function alias becomes void.  Void is
+ * allowed because the return type is never used.
+ *
+ * @typedef {function():undefined}
  */
-undefinedns.a;
+undefinedns.FuncReturnsUndefAlias;
 
 /**
- * @const {void}
+ * undefined in a union type returned by a function alias remains undefined to
+ * avoid void values, but means some uses of aliases will need to explicitly
+ * return undefined in TypeScript.
+ *
+ * @typedef {function():(number | undefined)}
  */
-undefinedns.b;
+undefinedns.FuncReturnsUndefUnionAlias;
 
 /**
- * @const {undefined | string}
+ * A type alias for a union including undefined remains undefined.
+ *
+ * @typedef {boolean | undefined}
  */
-undefinedns.c;
+undefinedns.UndefUnionAlias;
 
 /**
+ * Fields using undefined or void become optional fields.  The nullable field
+ * remains a union with null.
+ *
+ * @typedef {{
+ *   foo: (boolean | undefined),
+ *   bar: (boolean | void),
+ *   baz: (boolean | null),
+ * }}
+ */
+undefinedns.UndefUnionRecord;
+
+/**
+ * A return type that includes undefined remains undefined in TS instead of
+ * becoming void, because the return value of this function may be assigned to
+ * a value.
+ *
+ * @return {undefined | string}
+ */
+undefinedns.funcReturnsUndefUnion = function() {};
+
+/**
+ * A function that returns undefined returns void in TypeScript.
+ *
+ * This is safe because the return value is unused.
+ *
+ * @return {undefined}
+ */
+undefinedns.funcReturnsUndefined = function() {};
+
+
+/**
+ * A function that returns void returns void in TypeScript.
+ *
+ * Similar to funcReturnsUndefined above.
+ *
+ * @return {void}
+ */
+undefinedns.funcReturnsVoid = function() {};
+
+/**
+ * A function parameter that matches a predefined alias will be emitted as
+ * that alias, meaning it will still return undefined.
+ *
+ * @param {function():(number | undefined)} funcAlias
+ */
+undefinedns.matchesFuncAlias = function(funcAlias) {};
+
+/**
+ * A function accepting and returning a union type including undefined will
+ * still return undefined in TypeScript.
+ *
+ * This is to avoid void values in TypeScript code.
+ *
+ * @param {undefinedns.UndefUnionAlias} union
+ * @return {undefinedns.UndefUnionAlias}
+ */
+undefinedns.returnsUndefUnionAlias = function(union) {};
+
+/**
+ * A function parameter that returns a type alias that includes undefined will
+ * still return undefined in TypeScript.
+ *
+ * This requires passing a function that explicitly returns undefined, but
+ * since there's no way to know how the alias is used otherwise, it's not safe
+ * to emit void.
+ *
+ * @param {function(): undefinedns.UndefUnionAlias} undefAliasFunc
+ */
+undefinedns.takesUndefAliasFunc = function(undefAliasFunc) {};
+
+/**
+ * A function parameter that returns undefined returns void in TypeScript.
+ *
+ * This is safe because the return value is unused. Note that this matches
+ * FuncReturnsUndefAlias, so that alias is used for the type in TypeScript.
+ *
+ * @param {function(): undefined} undefFunc
+ */
+undefinedns.takesUndefFunc = function(undefFunc) {};
+
+/**
+ * A record parameter with a function that returns undefined returns void in
+ * TypeScript.
+ *
+ * This is safe for similar reasons to the above.  Note that this matches the
+ * FuncReturnsUndefAlias, so that alias is used in TypeScript.
+ *
+ * @param {{func: function():undefined}} undefFuncRecord
+ */
+undefinedns.takesUndefFuncRecord = function(undefFuncRecord) {};
+
+/**
+ * A function that returns a union type including undefined as a parameter
+ * returns undefined in TypeScript.
+ *
+ * Clutz will only emit void for functions that only return undefined, to
+ * avoid having void value types.
+ *
+ * TODO(b/162851479): Allow void here since this function is invoked from
+ * JavaScript, where the void return type is treated as undefined.
+ *
+ * @param {function():(string|undefined)} undefUnionFunc
+ */
+undefinedns.takesUndefUnionFunc = function(undefUnionFunc) {};
+
+/**
+ * A function parameter that returns void returns void in TypeScript.
+ *
+ * This allows passing functions that omit a return value, and is safe because
+ * the return value of the parameter function is not used.
+ *
+ * @param {function(): void} voidFunc
+ */
+undefinedns.takesVoidFunc = function(voidFunc) {};
+
+/**
+ * Omitting the return type of a function causes it to return void in TS.
+ *
  * @param {undefined} a
  * @param {undefined | string} b
  * @param {string=} c
  */
-undefinedns.f = function(a, b, c) {};
+undefinedns.undefParamsNoReturn = function(a, b, c) {};
 
 /**
- * @return {undefined}
+ * Undefined values in JS become undefined in TypeScript.
+ *
+ * @const {undefined}
  */
-undefinedns.g = function() {};
-
+undefinedns.undefProp;
 
 /**
- * @return {void}
+ * Undefined in a union value remains undefined.
+ *
+ * @const {undefined | string}
  */
-undefinedns.h = function() {};
+undefinedns.undefUnionProp;
 
 /**
- * @return {undefined | string}
+ * Void values in JS become undefined in TypeScript.
+ *
+ * Void in JS is a synonym for undefined, but is a distinct type in TypeScript
+ * that's not interchangeable with undefined values.
+ *
+ * @const {void}
  */
-undefinedns.i = function() {};
+undefinedns.voidProp;
 
-/**
- * @typedef {{foo: (boolean | undefined)}}
- */
-undefinedns.alias;
-
-
-/**
- * @param {function(): void} f
- * @param {function(): (string|void)} g
- * @constructor
- */
-undefinedns.C = function(f, g) {};
